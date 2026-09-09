398049e3dd48099c7fad0b48dd5aa77063fb2686
Pablo Durán <pabloduranballesteros@gmail.com>
2026-09-09 12:50:13 +0200

    Terminado diseño landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './presentation/pages/landing/landing.component';
 import { DashboardComponent } from './presentation/pages/dashboard/dashboard.component';
+import { LoginComponent } from './presentation/pages/login/login.component';
+import { RegisterComponent } from './presentation/pages/register/register.component';
 
 export const routes: Routes = [
      {path: '', component:LandingComponent},
      {path:'dashboard',component:DashboardComponent},
+     {path:'login',component:LoginComponent},
+     {path:'registro',component:RegisterComponent},
+
       
 ];
